7ab6b8a1cad91a42e2d8a92790757c5de48ab212
Oscar España <espaaoscar@gmail.com>
2021-01-30 09:39:46 -0500

    feat: creación de las páginas: gracias y cancelado

---
+++ src/pages/cancelado.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { SEO } from "../components"
+import { Button, Purchase } from "../styles/components"
+import { Link } from "gatsby"
+
+const cancelado = () => {
+  return (
+    <div>
+      <SEO title="Compra Cancelada" />
+      <Purchase>
+        <h2>Compra Cancelada</h2>
+        <p>Lamentamos que no hayas podido comprar tu swag</p>
+        <p>!Te esperamos de vuelta! </p>
+        <span rol="img" aria-label="emoji">
+          💖💖
+        </span>
+        <Link to="/">
+          <Button>Volver al Catálogo</Button>
+        </Link>
+      </Purchase>
+    </div>
+  )
+}
+
+export default cancelado
