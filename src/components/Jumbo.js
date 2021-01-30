c48d21a3c64e05b0c172e77638378e7a82be0518
Oscar España <espaaoscar@gmail.com>
2021-01-30 12:06:42 -0500

    feat: añadir query de graphql y usarla en la descripción de la página de inicio

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -1,11 +1,11 @@
 import React from "react"
 import { StyledJumbo } from "../styles/components"
-const Jumbo = () => {
+const Jumbo = ({ description }) => {
   return (
     <StyledJumbo>
       <div>
         <h2>Consigue el mejor swag exclusivo de Platzi</h2>
-        <small>Piezas eleganes para los mejores estudiantes!</small>
+        <small>{description}</small>
       </div>
     </StyledJumbo>
   )
