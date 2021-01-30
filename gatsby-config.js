151a4b91be877b112534300fa0dbbba9bcdb3c2b
Oscar España <espaaoscar@gmail.com>
2021-01-29 23:39:28 -0500

    feat: commit inicial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `PlatziSwag`,
+    description: `El mejor swag de Platzi disponible para ti.`,
+    author: `Oscar España`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
