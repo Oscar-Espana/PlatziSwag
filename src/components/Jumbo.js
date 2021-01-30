e1c9ff9ae03e4d8c1ba24b73856147d9ff5fa94c
Oscar España <espaaoscar@gmail.com>
2021-01-30 09:14:31 -0500

    feat: creación del component Jumbo y uso en la página index

---
+++ src/components/Jumbo.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { StyledJumbo } from "../styles/components"
+const Jumbo = () => {
+  return (
+    <StyledJumbo>
+      <div>
+        <h2>Consigue el mejor swag exclusivo de Platzi</h2>
+        <small>Piezas eleganes para los mejores estudiantes!</small>
+      </div>
+    </StyledJumbo>
+  )
+}
+
+export default Jumbo
